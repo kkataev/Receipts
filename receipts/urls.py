690ae735a04066256730e5b9f8f0d29052424e43
Kirill Kataev <kirilkataev@gmail.com>
2017-06-03 03:40:43 +0300

    Try to launch static

--- receipts/urls.py
+++ receipts/urls.py
@@ -6,10 +6,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^helloworld/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': path.join(path.dirname(__file__), 'static')}),
 ]
-urlpatterns += patterns('',
-	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes':True}),
-)
+
 
 if settings.DEBUG:
     import debug_toolbar
