5da852066cae242933e386d6d8add50791282107
Kirill Kataev <kirilkataev@gmail.com>
2017-06-03 16:10:00 +0300

    add debug_toolbar middleware

--- receipts/settings.py
+++ receipts/settings.py
@@ -45,6 +45,8 @@ class Common(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+
     ]
 
     ROOT_URLCONF = 'receipts.urls'
