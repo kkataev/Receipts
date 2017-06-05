db45e7e3284b2557c7f58b691a8c2ac101766824
Kirill Kataev <kirilkataev@gmail.com>
2017-06-06 00:34:59 +0300

    Remove rest django auth

--- admin/urls.py
+++ admin/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^receipts/', include('receipts.urls')),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^login', views.OnePageAppView.as_view(), name='home'),
     url(r'^api/auth/$', views.AuthView.as_view(), name='authenticate')
 ]
