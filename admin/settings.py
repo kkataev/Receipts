d6df7815c66cd853182c732b9fae09f8abc74604
Kirill Kataev <kirill.kataev@genesys.com>
2017-06-21 13:51:33 +0300

    Turn off ssl

--- admin/settings.py
+++ admin/settings.py
@@ -160,9 +160,9 @@ class Staging(Common):
     SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
     SECURE_REDIRECT_EXEMPT = values.ListValue([])
     SECURE_SSL_HOST = values.Value(None)
-    #SECURE_SSL_REDIRECT = values.BooleanValue(False)
+    SECURE_SSL_REDIRECT = values.BooleanValue(False)
     #SECURE_PROXY_SSL_HEADER = values.TupleValue(
-    #    ('HTTP_X_FORWARDED_PROTO', 'https')
+    #   ('HTTP_X_FORWARDED_PROTO', 'https')
     #)
 
 
