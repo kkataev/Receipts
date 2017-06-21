2a376cf115bec440d7d87c563325724a76be08fd
Kirill Kataev <kirill.kataev@genesys.com>
2017-06-21 14:19:47 +0300

    Enable ssl to auth work

--- admin/settings.py
+++ admin/settings.py
@@ -160,10 +160,10 @@ class Staging(Common):
     SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
     SECURE_REDIRECT_EXEMPT = values.ListValue([])
     SECURE_SSL_HOST = values.Value(None)
-    SECURE_SSL_REDIRECT = values.BooleanValue(False)
-    #SECURE_PROXY_SSL_HEADER = values.TupleValue(
-    #   ('HTTP_X_FORWARDED_PROTO', 'https')
-    #)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    SECURE_PROXY_SSL_HEADER = values.TupleValue(
+        ('HTTP_X_FORWARDED_PROTO', 'https')
+    )
 
 
 class Production(Staging):
