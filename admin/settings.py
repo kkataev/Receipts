f5f90a6e3a4a3ed44255bf04b79e8b3194ed9217
Kirill Kataev <kirill.kataev@genesys.com>
2017-06-21 13:40:54 +0300

    Turn off ssl, turn on debug

--- admin/settings.py
+++ admin/settings.py
@@ -160,15 +160,15 @@ class Staging(Common):
     SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
     SECURE_REDIRECT_EXEMPT = values.ListValue([])
     SECURE_SSL_HOST = values.Value(None)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
-    SECURE_PROXY_SSL_HEADER = values.TupleValue(
-        ('HTTP_X_FORWARDED_PROTO', 'https')
-    )
+    #SECURE_SSL_REDIRECT = values.BooleanValue(False)
+    #SECURE_PROXY_SSL_HEADER = values.TupleValue(
+    #    ('HTTP_X_FORWARDED_PROTO', 'https')
+    #)
 
 
 class Production(Staging):
 
-    DEBUG = False
+    DEBUG = True
     """
     The in-production settings.
     """
